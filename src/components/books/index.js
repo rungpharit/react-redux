16057f668b8e72ea0dff9792fb81974d325e8c57
Ong <Ong@gmail.com>
2020-05-01 16:15:14 +0700

    redux-book

--- src/components/books/index.js
+++ src/components/books/index.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addBook , buyBook } from '../../redux/book/bookAction'
 
 class Book extends Component {
   render() {
     return (
       <div>
-        <h2>Number of Books - </h2>
+        <h2>Number of Books - {this.props.numOfBooks}</h2>
         <div>
-          <button>Add Book</button>
-          <button>Buy Book</button>
+          <button onClick={() => {this.props.addBook()}}>Add Book</button>
+          <button onClick={() => {this.props.buyBook()}}>Buy Book</button>
         </div>
       </div>
     )
   }
 }
 
-export default  Book;
+const mapStateToProps = state => {
+  return {
+    numOfBooks:state.numOfBooks
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addBook : () => dispatch(addBook()),
+    buyBook : () => dispatch(buyBook())
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Book);
